67b41ce7e6b5dbc2d6a02781c4df0da1b7b21b0a
Masahiro Rikiso <mshrrks@gmail.com>
2017-08-30 13:47:30 +0900

    add RANSAC plane segmentation

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -41,7 +41,24 @@ cloud_filtered = passthrough.filter()
 filename = 'pass_through_filtered.pcd'
 pcl.save(cloud_filtered, filename)
 
-# RANSAC plane segmentation
+'''
+RANSAC plane segmentation: Chap4-3-14
+'''
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
+
+# Set the model you wish to fit 
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+# Experiment with different values for max_distance 
+# for segmenting the table
+max_distance = 1
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
 
 
 # Extract inliers
